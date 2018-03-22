037fcd2f865feee47dd028fa248152d11c7e11b7
Teuku Atjeh <thilmanrevanda@gmail.com>
2018-03-23 00:35:30 +0700

    change pre loader

--- puma-web/src/App.vue
+++ puma-web/src/App.vue
@@ -1,16 +1,10 @@
 <template>
   <div id="app">
     <div v-if="loading">
-      <div class="ipl-progress-indicator" id="ipl-progress-indicator" ref="ipl-progress-indicator">
-        <div class="ipl-progress-indicator-head">
-          <div class="first-indicator"></div>
-          <div class="second-indicator"></div>
-        </div>
-        <div class="insp-logo-frame">
-          <center>
-            <b-img fluid src="https://iqyoe.github.io/puma-web/assets/img/horizontal-logo-comp-lengkap-white.png" alt="PUMA Computing Logo" /></center>
-        </div>
-      </div>
+    <div id="block-loader">
+        <div class="loader"></div>
+        <img class="loader-img" src="../static/computing.png">  
+    </div>
     </div>
     <div v-else>
     <navi></navi>
@@ -46,84 +40,47 @@ export default {
 </script>
 
 <style>
-[v-cloak] {
-  display: none;
-}
-    .ipl-progress-indicator.available {
-    opacity: 0;
-    }
-    .ipl-progress-indicator {
-    background-color: #000;
+#block-loader{
     width: 100%;
     height: 100%;
+    background: rgba(0,0,0,1.0);
     position: fixed;
-    opacity: 1;
-    pointer-events: none;
-    -webkit-transition: opacity cubic-bezier(.4, 0, .2, 1) 436ms;
-    -moz-transition: opacity cubic-bezier(.4, 0, .2, 1) 436ms;
-    transition: opacity cubic-bezier(.4, 0, .2, 1) 436ms;
-    z-index: 9999;
-    }
-    .insp-logo-frame {
-    display: -webkit-flex;
-    display: -moz-flex;
-    display: flex;
-    -webkit-flex-direction: column;
-    -moz-flex-direction: column;
-    flex-direction: column;
-    -webkit-justify-content: center;
-    -moz-justify-content: center;
-    justify-content: center;
-    -webkit-animation: fadein 436ms;
-    -moz-animation: fadein 436ms;
-    animation: fadein 436ms;
-    height: 98%;
-    }
-    .insp-logo-frame-img{
-    width: 112px;
-    height: 112px;
-    -webkit-align-self: center;
-    -moz-align-self: center;
-    align-self: center;
-    border-radius: 50%;
-    }
-    .ipl-progress-indicator-head {
-    background-color: rgba(255,255,255,.5);
-    height: 4px;
-    overflow: hidden;
-    position: relative;
-    }
-    .ipl-progress-indicator .first-indicator, .ipl-progress-indicator .second-indicator {
-    background-color: rgba(255,255,255,1);
-    bottom: 0;
-    left: 0;
-    right: 0;
-    top: 0;
-    position: absolute;
-    -webkit-transform-origin: left center;
-    -moz-transform-origin: left center;
-    transform-origin: left center;
-    -webkit-transform: scaleX(0);
-    -moz-transform: scaleX(0);
-    transform: scaleX(0);
-    }
-    .ipl-progress-indicator .first-indicator {
-    -webkit-animation: first-indicator 2000ms linear infinite;
-    -moz-animation: first-indicator 2000ms linear infinite;
-    animation: first-indicator 2000ms linear infinite;
-    }
-    .ipl-progress-indicator .second-indicator {
-    -webkit-animation: second-indicator 2000ms linear infinite;
-    -moz-animation: second-indicator 2000ms linear infinite;
-    animation: second-indicator 2000ms linear infinite;
-    }
-    .ipl-progress-indicator .insp-logo {
-    /* animation: App-logo-spin infinite 20s linear; */
-    border-radius: 50%;
-    -webkit-align-self: center;
-    -moz-align-self: center;
-    align-self: center;
 }
+.loader {
+  border: 16px solid #000;
+  border-radius: 50%;
+  border-top: 10px solid #fff;
+  border-bottom: 10px solid #fff;
+  width: 140px;
+  height: 140px;
+  -webkit-animation: spin 0.5s linear infinite;
+  animation: spin 0.5s linear infinite;
+  position: absolute;
+  z-index: 15;
+  top: 50%;
+  left: 50%;
+  margin: -70px 0 0 -70px;
+}
+
+/* Safari */
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+.loader-img{
+    width: 80px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin: -40px 0 0 -40px;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
